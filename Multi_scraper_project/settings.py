a814401a1b9caaa4cf34584107a1e9e31e11d362
hari kumar <harikumar809816@gmail.com>
2025-11-25 10:25:15 +0530

    Update database config to SQLite with auto-detect

--- Multi_scraper_project/settings.py
+++ Multi_scraper_project/settings.py
@@ -75,6 +75,7 @@ SESSION_COOKIE_SAMESITE = 'Lax'  # CSRF protection
 
 
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     "default": {
@@ -82,7 +83,6 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
